245786a9fea714b3afb81b5745e8bc7d7a4c33e8
Konstantin <kss47242>
2020-03-10 14:56:59 +0300

    start

---
+++ src/main/java/com/example/RockingChairApplication.java
@@ -0,0 +1,13 @@
+package com.example.rocking_chair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockingChairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockingChairApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RockingChairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rocking_chair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockingChairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
